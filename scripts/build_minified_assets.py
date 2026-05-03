8224bcfe01435e26ac5dadcbd0a0953ed9052df4
lyuf09 <feier530@icloud.com>
2026-05-03 17:38:51 -0400

    Stagger multi-art music detail layouts

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260503-music-featured-mobile-layout1"
+VERSION = "20260503-music-multiart-collage1"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
