82279fe9af6844d03e0e1a4fd971e4368223b830
lyuf09 <feier530@icloud.com>
2026-05-03 00:44:24 -0400

    Refine mobile layouts for music and photography

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260503-featured-cover-tune"
+VERSION = "20260503-mobile-layout-pass"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
