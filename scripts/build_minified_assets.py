29230d26b314bdf5b62ba6bfc674c858d6ce9c6e
lyuf09 <feier530@icloud.com>
2026-05-03 01:10:26 -0400

    Fix He and Me featured cover crop

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260503-player-mobile-pass3"
+VERSION = "20260503-heandme-cover-fix"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
