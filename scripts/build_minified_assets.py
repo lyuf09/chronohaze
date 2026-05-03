b9d79bbed16dcc859d23385d33d5722acfc94136
lyuf09 <feier530@icloud.com>
2026-05-02 22:08:11 -0400

    Make music detail pages more editorial

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260502-featured-copy-fill"
+VERSION = "20260502-music-detail-editorial"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
