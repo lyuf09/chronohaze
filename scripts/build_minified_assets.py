9c9a1485e1d829de8e9842c4342836d2a2f96736
lyuf09 <feier530@icloud.com>
2026-05-03 13:51:26 -0400

    Tune Affizieren featured cover treatment

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260503-mobile-sitewide-pass2"
+VERSION = "20260503-affizieren-feature-tune"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
