a3e36ca2b85a93a56fd7de255d9973db31ad2358
lyuf09 <feier530@icloud.com>
2026-05-03 01:20:32 -0400

    Polish mobile layouts across site

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260503-heandme-cover-fix"
+VERSION = "20260503-mobile-polish-pass1"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
