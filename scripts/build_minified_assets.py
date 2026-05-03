f70d74aee679ba3d563a53b605b94161d6c831d4
lyuf09 <feier530@icloud.com>
2026-05-02 21:44:58 -0400

    Make photography homepage more editorial

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260502-page-swap-fresh"
+VERSION = "20260502-photo-home-editorial"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
