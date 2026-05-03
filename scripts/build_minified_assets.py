cc0d3d7870f991b092d26002772d4e40ad61a664
lyuf09 <feier530@icloud.com>
2026-05-03 13:37:37 -0400

    Polish mobile experience across site

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260503-orchid-cover-unified"
+VERSION = "20260503-mobile-sitewide-pass2"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
