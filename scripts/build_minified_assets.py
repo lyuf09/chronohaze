7812136fbf76b6451f9a050abc22f4be67465a25
lyuf09 <feier530@icloud.com>
2026-05-03 14:19:50 -0400

    Adjust featured music cover crops

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260503-photo-en-intro-fix"
+VERSION = "20260503-featured-crop-pass4"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
