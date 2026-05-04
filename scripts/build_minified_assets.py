65ab479b188b2778273a77ef18279cb2e39cc889
lyuf09 <feier530@icloud.com>
2026-05-04 11:56:01 -0400

    Reframe photography page as visual essay

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260504-photo-featured-caption-pass1"
+VERSION = "20260504-photo-visual-essay-pass1"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
