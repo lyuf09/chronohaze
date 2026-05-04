b776fcb23fa4eeae91361568d1208bb2323467eb
lyuf09 <feier530@icloud.com>
2026-05-03 21:16:02 -0400

    Enlarge supporting images in photo detail layouts

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260503-music-listening-room1"
+VERSION = "20260503-photo-detail-scale-pass1"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
