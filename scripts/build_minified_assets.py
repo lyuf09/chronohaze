2f85fbc775b7eafdc953453ffdcee92b06e37693
lyuf09 <feier530@icloud.com>
2026-05-03 22:13:33 -0400

    Restore music layout and keep title refined

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260503-music-photo-layout-alignment2"
+VERSION = "20260503-music-title-only-revert1"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
