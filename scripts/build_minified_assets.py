17fb96f226f0ec0cb399e8015c1ff8ea131cc70d
lyuf09 <feier530@icloud.com>
2026-05-04 11:27:25 -0400

    Refine photo featured covers and captions

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260503-music-title-only-revert1"
+VERSION = "20260504-photo-featured-caption-pass1"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
