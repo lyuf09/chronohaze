f2fb5cb97e8caca269c51dbddf6a7daecf15cb2f
lyuf09 <feier530@icloud.com>
2026-05-05 15:43:25 -0400

    Reframe photography formative note

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260505-photo-statement-pass1"
+VERSION = "20260505-photo-formative-note-pass1"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
