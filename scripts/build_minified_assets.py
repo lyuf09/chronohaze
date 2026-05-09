036e7318fec8b58d866d9bbd87f27579b3dd7513
lyuf09 <feier530@icloud.com>
2026-05-09 15:20:50 -0400

    Add selected evidence section to homepage

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -5,7 +5,7 @@ import argparse
 import re
 from pathlib import Path
 
-VERSION = "20260509-photo-archive-titles1"
+VERSION = "20260509-home-evidence1"
 
 CRITICAL_LOADER_MARKER = "chronohaze-critical-loader-style"
 CRITICAL_LOADER_SNIPPET = """  <style id="chronohaze-critical-loader-style">
