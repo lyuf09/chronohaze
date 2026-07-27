2e1c9d06e47d493f4d2a3e326691e11d976c87c2
lyuf09 <feier530@icloud.com>
2026-07-27 15:11:13 +0800

    Keep mobile portrait cache version during builds

--- scripts/build_minified_assets.py
+++ scripts/build_minified_assets.py
@@ -6,7 +6,7 @@ import re
 from pathlib import Path
 
 VERSION = "20260723-information-architecture1"
-HOME_VERSION = "20260723-home-now1"
+HOME_VERSION = "20260727-mobile-portrait-static1"
 PROTECT_VERSION = "20260726-primary-nav-language1"
 MUSIC_PROTECT_VERSION = "20260726-primary-nav-language1"
 MUSIC_STYLE_VERSION = "20260723-information-architecture1"
